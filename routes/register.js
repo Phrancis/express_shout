47443bb3f46ce2651291bda0589ad70f6523aae8
Phrancis <Phrancis@users.noreply.github.com>
2017-03-17 16:50:48 -0400

    add submit functionality to register route

--- routes/register.js
+++ routes/register.js
@@ -1,4 +1,34 @@
 "use strict"
+const User = require("../lib/user")
+
 exports.form = function(req, res) {
   res.render("register", { title: "Register" })
+}
+
+exports.submit = function(req, res, next) {
+  const data = req.body.user
+  // check if user name is unique
+  User.getByName(data.name, function(err, user) {
+    // defer DB connection and other errors
+    if (err) return next(err)
+    // Redis will default it
+    if(user.id) {
+      res.error("Username already taken!")
+      res.redirect("back")
+    } else {
+      // create new user using POST data
+      user = new User({
+        name: data.name,
+        pass: data.pass
+      })
+      // save new user
+      user.save(function(err) {
+        if (err) return next(err)
+        // store uid for authentication
+        req.session.uid = user.id
+        // redirect to entry listing page
+        res.redirect("/")
+      })
+    }
+  })
 }
