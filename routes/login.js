a04f06359ecfb9fe248408c7f5c11a11f49feb3d
Phrancis <Phrancis@users.noreply.github.com>
2017-03-17 17:10:06 -0400

    add submit and logout functionality to login route

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,35 @@
 "use strict"
+
+const User = require("../lib/user")
+
 exports.form = function(req, res) {
   res.render("login", { title: "Login" })
+}
+
+exports.submit = function(req, res, next) {
+  const data = req.body.user
+  // check credentials
+  User.authenticate(data.name, data.pass, function(err, user) {
+    // delegate errors
+    if (err) return next(err)
+    // handle user with valid credentials
+    if (user) {
+      // store uid for authentication
+      req.session.uid = user.id
+      // redirect to entry listing
+      res.redirect("/")
+    } else {
+      // expose an error message
+      res.error("Sorry! Invalid credentials.")
+      // redirect back to login form
+      res.redirect("back")
+    }
+  })
+}
+
+exports.logout = function(req, res) {
+  req.session.destroy(function(err) {
+    if (err) throw err
+    res.redirect("/")
+  })
 }
