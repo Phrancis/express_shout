074f1ea0b1e9af85ae4d0bf502c401ac04ae0551
Phrancis <Phrancis@users.noreply.github.com>
2017-03-22 18:41:02 -0400

    refactor user query, add to register route

--- routes/register.js
+++ routes/register.js
@@ -8,28 +8,28 @@ exports.form = function(req, res) {
 exports.submit = function(req, res, next) {
   const data = req.body.user
   // check if user name is unique
-  User.getByName(data.name, function(err, user) {
+  let user = User.getDataByName(data.name)
+  //User.getDataByName(data.name, function(err, user) {
     // defer DB connection and other errors
-    if (err) return next(err)
-    // Redis will default it
+    //if (err) return next(err)
     // TODO "username already taken" check doesn't work, creating duplicate users into Redis is possible
-    if(user.id) {
-      res.error("Username already taken!")
-      res.redirect("back")
-    } else {
-      // create new user using POST data
-      user = new User({
-        name: data.name,
-        pass: data.pass
-      })
-      // save new user
-      user.save(function(err) {
-        if (err) return next(err)
-        // store uid for authentication
-        req.session.uid = user.id
-        // redirect to entry listing page
-        res.redirect("/")
-      })
-    }
-  })
+  if(user) {
+    res.error("Username already taken!")
+    res.redirect("back")
+  } else {
+    // create new user using POST data
+    user = new User({
+      name: data.name,
+      pass: data.pass
+    })
+    // save new user
+    user.save(function(err) {
+      if (err) return next(err)
+      // store uid for authentication
+      req.session.uid = user.id
+      // redirect to entry listing page
+      res.redirect("/")
+    })
+  }
+  //})
 }
